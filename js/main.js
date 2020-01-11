f6a242aa636971deee21c78757972a4a6fd74586
npksm <nick.pksm@gmail.com>
2020-01-11 16:01:43 +0530

    Begins again
    Trying to revamp site, trying to copy https://archillect.com/

---
+++ js/main.js
@@ -0,0 +1,23 @@
+﻿const s = io.connect(window.location.origin);
+s.on('x', x => typeof x == "string" ? eval(x) : console.log(x));
+
+function pop(m) {
+    let n = el("n");
+    if (!n) {
+        n = document.createElement('div');
+        n.setAttribute("id", "n");
+        document.body.appendChild(n);
+    }
+    n.innerHTML = m;
+    setTimeout(() => {
+        n.style.transform = "translateY(0)";
+    }, 50)
+    setTimeout(() => {
+        n.style.transform = "";
+    }, n.innerText.length * 50 + 2000)
+
+}
+
+function el(n) {
+    return document.getElementById(n);
+}
