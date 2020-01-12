bc70e863096cb0e69c59409d3a4f97bd87e600f9
npksm <nick.pksm@gmail.com>
2020-01-12 11:42:15 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,13 +7,14 @@ $.ajax({
 	type: 'GET',
 	data: { id, media_url, caption },
 	success: function (data) {
-		console.log(data);
-		//for (x in data.data) {
+		
+		for (x in data.data) {
+			console.log(data);
 			//$(".posts").append('<img src="' + data.data[x].media_url + '>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
 			// data.data[x].images.thumbnail.url - URL of image 150х150
 			// data.data[x].images.standard_resolution.url - URL of image 612х612
 			// data.data[x].link - Instagram post URL 
-		//}
+		}
 	},
 	error: function (data) {
 		console.log(data); // send the error notifications to console
