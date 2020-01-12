5d00d177b8b69988d18e3fe0f62f84c061bcb7c3
npksm <nick.pksm@gmail.com>
2020-01-12 11:40:15 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
 ﻿var token = 'IGQVJVdHNqSmdtMnpkSzZAvZAGpfZAWhPSktkQW1zS016b1V2TEJKRlJ2b2FBdVF4NEYzLWR1QjZAaRlhfcHU1QTZABM0xiR3FfTnJsSHBVU2JfSmNTYnVLUjdaeENOM0FtNmtTeFMxOHNrYzVKLWdaNVFFdTM0cEh0QTJsUUlJ',
-	userid = 17306901306, 
-	num_photos = 5; 
+	userid = 17306901306,  
 
 $.ajax({
 	url: 'https://graph.instagram.com/me/media?fields=id,media_url, caption&access_token=' + token, // or /users/self/media/recent for Sandbox
 	dataType: 'jsonp',
 	type: 'GET',
-	data: { count: num_photos },
+	data: { id, media_url, caption },
 	success: function (data) {
 		console.log(data);
-		for (x in data.data) {
-			$('ul').append('<li><img src="' + data.data[x].media_url + '></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
+		//for (x in data.data) {
+			//$(".posts").append('<img src="' + data.data[x].media_url + '>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
 			// data.data[x].images.thumbnail.url - URL of image 150х150
 			// data.data[x].images.standard_resolution.url - URL of image 612х612
 			// data.data[x].link - Instagram post URL 
-		}
+		//}
 	},
 	error: function (data) {
 		console.log(data); // send the error notifications to console
