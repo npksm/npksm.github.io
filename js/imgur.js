3e1531668aef26dcb2ec7631588975ac45a4af34
npksm <nick.pksm@gmail.com>
2020-01-12 21:20:39 +0530

    Update imgur.js

--- js/imgur.js
+++ js/imgur.js
@@ -8,7 +8,9 @@ var settings = {
 };
 
 $.ajax(settings).done(function (response) {
-				for (x in response){			
+	var x = response.length;
+	
+				for (x in response.data){			
 					$('#posts').html('<a class="post"><img class="thumb" src="'+response.data[x].link+'"></a>');
 					//$('#imagepost').html('<img id="ii" src="'+response.data[x].link+'"></img>');
 					//$('#words').html('<p> '+response.data[x].name+'></p>');		
