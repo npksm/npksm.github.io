d466c9ca49a3b57d6691a06204388eb2b26a895f
npksm <nick.pksm@gmail.com>
2020-01-12 11:49:37 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 ﻿var token = 'IGQVJVdHNqSmdtMnpkSzZAvZAGpfZAWhPSktkQW1zS016b1V2TEJKRlJ2b2FBdVF4NEYzLWR1QjZAaRlhfcHU1QTZABM0xiR3FfTnJsSHBVU2JfSmNTYnVLUjdaeENOM0FtNmtTeFMxOHNrYzVKLWdaNVFFdTM0cEh0QTJsUUlJ',
-	userid = 17306901306;  
+	userid = 17306901306,
+	id,
+	media_url,
+	caption;  
 
 $.ajax({
 	url: 'https://graph.instagram.com/me/media?fields=id,media_url, caption&access_token=' + token, // or /users/self/media/recent for Sandbox
