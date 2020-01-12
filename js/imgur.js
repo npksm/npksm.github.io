7ede5b80db0532c4314ddd7bcad2038d9f624df5
npksm <nick.pksm@gmail.com>
2020-01-12 18:56:47 +0530

    reducing some css

--- js/imgur.js
+++ js/imgur.js
@@ -9,7 +9,7 @@ var settings = {
 
 $.ajax(settings).done(function (response) {
 				for( x in response.data){
-					$('#imgurphotos').html('<a><img class="thumb" src="'+response.data[x].link+'"></img></a>');
+					$('#posts').html('<a class="post"><img class="thumb" src="'+response.data[x].link+'"></a>');
 					//$('#imagepost').html('<img id="ii" src="'+response.data[x].link+'"></img>');
 					//$('#words').html('<p> '+response.data[x].name+'></p>');
 					console.log(response.data);
