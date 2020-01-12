068677380e36705de7b9250e1086e14310cbbde7
npksm <nick.pksm@gmail.com>
2020-01-12 12:20:30 +0530

    switching to just .get

--- js/main.js
+++ js/main.js
@@ -4,23 +4,9 @@
 	media_url,
 	caption;  
 
-$.ajax({
-	url: 'https://graph.instagram.com/me/media?fields=id,media_url, caption&access_token=' + token, // or /users/self/media/recent for Sandbox
-	dataType: 'jsonp',
-	type: 'GET',
-	data: { id, media_url, caption },
-	success: function (data) {
-		console.log(data);
-		$('.posts').append("<p>success I suppose</p>");
-		//for (x in data.data) {
-			//$(".posts").append('<img src="' + data.data[x].media_url + '>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
-			// data.data[x].images.thumbnail.url - URL of image 150х150
-			// data.data[x].images.standard_resolution.url - URL of image 612х612
-			// data.data[x].link - Instagram post URL 
-		//}
-	},
-	error: function (data) {
-		$('.posts').append("<p>failure</p>");
-		console.log(data); // send the error notifications to console
-	}
-});
+$.get(
+	'https://graph.instagram.com/me/media?fields=id,media_url, caption&access_token=' + token,
+	function(data, status){
+		alert("Status: " + status);
+		$("posts").append(<p> "Data: " + data);
+	});
