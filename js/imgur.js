0b45cf8a9a91b6ca50bb7fe9f8c6e49506012b1c
npksm <nick.pksm@gmail.com>
2020-01-12 15:30:19 +0530

    moving on to imgur

---
+++ js/imgur.js
@@ -0,0 +1,12 @@
+var settings = {
+  "url": "https://api.imgur.com/3/account/me/images",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Authorization": "Bearer 27d067e9b81601874a49408b277dbd4641ae825c"
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
