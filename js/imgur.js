6b3df1d1c0934a4fab4d3c0dceba1b5e346ff44f
npksm <nick.pksm@gmail.com>
2020-01-12 20:09:38 +0530

    Update imgur.js

--- js/imgur.js
+++ js/imgur.js
@@ -8,12 +8,11 @@ var settings = {
 };
 
 $.ajax(settings).done(function (response) {
-
 				for( x in response.data){			
 					$('#posts').html('<a class="post"><img class="thumb" src="'+response.data[x].link+'"></a>');
 					//$('#imagepost').html('<img id="ii" src="'+response.data[x].link+'"></img>');
 					//$('#words').html('<p> '+response.data[x].name+'></p>');		
-					console.log(response.data[x].name);
+					console.log(response.data);
 				}
 
 });
