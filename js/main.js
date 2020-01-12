947ce159f8d623f747a821fb45ecb58d756419e1
npksm <nick.pksm@gmail.com>
2020-01-12 11:26:50 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 	num_photos = 5; 
 
 $.ajax({
-	url: 'https://api.instagram.com/v1/users/' + self + '/media/recent', // or /users/self/media/recent for Sandbox
+	url: 'https://graph.instagram.com/me/media?fields=id,media_url, caption&access_token=' + token, // or /users/self/media/recent for Sandbox
 	dataType: 'jsonp',
 	type: 'GET',
 	data: { access_token: token, count: num_photos },
 	success: function (data) {
 		console.log(data);
 		for (x in data.data) {
-			$('ul').append('<li><img src="' + data.data[x].images.low_resolution.url + '"></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
+			$('ul').append('<li><img src="' + data.data[x].media_url + '"></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
 			// data.data[x].images.thumbnail.url - URL of image 150х150
 			// data.data[x].images.standard_resolution.url - URL of image 612х612
 			// data.data[x].link - Instagram post URL 
