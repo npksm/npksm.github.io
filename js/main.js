b43d6a22267f54a694ecaa861524b91dae40095b
npksm <nick.pksm@gmail.com>
2020-01-12 11:51:49 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ $.ajax({
 	data: { id, media_url, caption },
 	success: function (data) {
 		console.log(data);
+		$(".posts").append('success I suppose');
 		//for (x in data.data) {
 			//$(".posts").append('<img src="' + data.data[x].media_url + '>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
 			// data.data[x].images.thumbnail.url - URL of image 150х150
@@ -19,6 +20,7 @@ $.ajax({
 		//}
 	},
 	error: function (data) {
+		$.(".posts").append('failure');
 		console.log(data); // send the error notifications to console
 	}
 });
