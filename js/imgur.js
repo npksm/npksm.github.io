fe11eda3b1a6a823c8d4e4e881a53fc2904b607e
npksm <nick.pksm@gmail.com>
2020-01-13 22:28:58 +0530

    working! I think

--- js/imgur.js
+++ js/imgur.js
@@ -10,7 +10,7 @@ var settings = {
 $.ajax(settings).done(function (response) {
 	console.log(response)	
 				for (x in response.data){			
-					$('#posts').html('<a class="post"><img class="thumb" src="'+response.data[x].link+'"></a>');
+					$('#posts').append('<a class="post"><img class="thumb" src="'+response.data[x].link+'"></a>');
 					//$('#imagepost').html('<img id="ii" src="'+response.data[x].link+'"></img>');
 					//$('#words').html('<p> '+response.data[x].name+'></p>');			
 	}
