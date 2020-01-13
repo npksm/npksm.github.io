6d2caca0ec43b6689ade708280597615e2d15410
npksm <nick.pksm@gmail.com>
2020-01-14 02:28:26 +0530

    nah

--- js/post.js
+++ js/post.js
@@ -4,7 +4,7 @@ $.ajax({
 	//dataType: "text",
 		success: function (response){
 				console.log('the page was loaded', response);
-				//$('#postimg').append(response);
+				$('#postimg').load(response section#posts );
 		},
 		error: function(error){
 				console.log('the page was NOT loaded', error);
