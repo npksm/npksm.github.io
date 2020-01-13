34d250ea0147d1126d4d15f6e025a12b441f3d6b
npksm <nick.pksm@gmail.com>
2020-01-14 01:54:21 +0530

    Update post.js

--- js/post.js
+++ js/post.js
@@ -1,5 +1,3 @@
-//function postImage
-
 function getPage(){
 	$.ajax({
 		url:"imagepost.html",
@@ -7,13 +5,31 @@ function getPage(){
 		dataType: "text",
 			success: function (response){
 					console.log('the page was loaded', response);
+					$('.content').html(response);
 			},
 			error: function(error){
 					console.log('the page was NOT loaded', error);
 			},
 			complete: function(xhr, status){
-					console.log("The request is complete!");
+					console.log("The get request is complete!");
 			}			
+	})
+};
+
+function postImage(){
+	$.ajax({
+		url:"imagepost.html",
+		type:"POST",
+		dataType: "text",
+			success: function (response){
+					console.log('the page was posted', response);
+			},
+			error: function(error){
+					console.log('the page was NOT posted', error);
+			},
+			complete: function(xhr, status){
+					console.log("The post request is complete!");
+			}
 	})
 };
 
