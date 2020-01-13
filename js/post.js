d8e3be93831d22fe841fda4281c5694070da29d2
npksm <nick.pksm@gmail.com>
2020-01-13 21:50:09 +0530

    Update post.js

--- js/post.js
+++ js/post.js
@@ -9,7 +9,7 @@ var settings = {
 
 $.ajax(settings).done(function (response) {		
 	$.each(response, function(index, value){
-		console.log("index: " +index+" value: " + value+ " value.name " +value.name) 
+		console.log("index: " +index+"\n value: " + value+ "\n data.name " +data.name) 
 		//$('#albums').html('<p>'response.data[x].name'</p>');
 		});
 });
