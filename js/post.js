47a9433891dd5e874222184aee6bda4ea19a0244
npksm <nick.pksm@gmail.com>
2020-01-14 02:14:45 +0530

    fiddling

--- js/post.js
+++ js/post.js
@@ -1,17 +1,17 @@
-	$.ajax({
-		url:"https://npksm.github.io/imagepost.html",
-		type: "GET",
-		dataType: "text",
-			success: function (response){
-					console.log('the page was loaded', response);
-					$('#content').append(response);
-			},
-			error: function(error){
-					console.log('the page was NOT loaded', error);
-			},
-			complete: function(xhr, status){
-					console.log("The get request is complete!");
-			}			
+$.ajax({
+	url:"https://npksm.github.io",
+	type: "GET",
+	dataType: "text",
+		success: function (response){
+				console.log('the page was loaded', response);
+				$('#postimg').append(response);
+		},
+		error: function(error){
+				console.log('the page was NOT loaded', error);
+		},
+		complete: function(xhr, status){
+				console.log("The get request is complete!");
+		}			
 	});
 
 	
