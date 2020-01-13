cc88ad30dd580f1990e2b2df58a5902f04942c6d
npksm <nick.pksm@gmail.com>
2020-01-14 00:59:07 +0530

    fix m later

--- js/post.js
+++ js/post.js
@@ -4,7 +4,7 @@ $.ajax({
 		dataType: "text",
 			success: function (response){
 					console.log('the page was loaded', response);
-
+					$('.content').html(response);
 			},
 			error: function(error){
 					console.log('the page was NOT loaded', error);
