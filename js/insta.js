5e8b4d5668c4cb693b8ab99e296fe2c4fcfd7484
npksm <nick.pksm@gmail.com>
2020-01-13 19:47:21 +0530

    still trying to fix imgur

--- js/insta.js
+++ js/insta.js
@@ -1,12 +1,11 @@
 ﻿var token = 'IGQVJVdHNqSmdtMnpkSzZAvZAGpfZAWhPSktkQW1zS016b1V2TEJKRlJ2b2FBdVF4NEYzLWR1QjZAaRlhfcHU1QTZABM0xiR3FfTnJsSHBVU2JfSmNTYnVLUjdaeENOM0FtNmtTeFMxOHNrYzVKLWdaNVFFdTM0cEh0QTJsUUlJ',
-	userid = 17306901306,
-	id,
-	media_url,
-	caption;  
+	userid = 17306901306;
 
 $.get(
 	'https://graph.instagram.com/me/media?fields=id,media_url, caption&access_token=' + token,
-	function(data, status){
-		alert("Data: " + data + "\nStatus: " + status);
-		//$("posts").append("<p> "Data: "+ data </p>");
+	function(data){
+		for (x in data){
+			$('#instaposts').html('<a class="post"><img class="thumb" src="'+data.data[x].media_url+'"></a>');
+		}
+		
 	});
