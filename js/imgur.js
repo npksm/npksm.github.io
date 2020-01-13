b7a7c8fc0d7cee1c7ce588490b0d66765b128c7b
npksm <nick.pksm@gmail.com>
2020-01-13 21:36:50 +0530

    second function

--- js/imgur.js
+++ js/imgur.js
@@ -10,7 +10,6 @@ var settings = {
 $.ajax(settings).done(function (response) {
 	console.log(response)	
 				for (x in response.data){			
-					
 					$('#posts').html('<a class="post"><img class="thumb" src="'+response.data[x].link+'"></a>');
 					//$('#imagepost').html('<img id="ii" src="'+response.data[x].link+'"></img>');
 					//$('#words').html('<p> '+response.data[x].name+'></p>');			
---
+++ js/post.js
@@ -0,0 +1,15 @@
+var settings = {
+  "url": "https://api.imgur.com/3/account/me/images",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Authorization": "Bearer 27d067e9b81601874a49408b277dbd4641ae825c"
+  },
+};
+
+$.ajax(settings).done(function (response) {		
+	$.each(response, function(index, value){
+		console.log("index: " +index+" value: " + value) 
+		//$('#albums').html('<p>'response.data[x].name'</p>');
+		});
+});
