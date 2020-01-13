6dba74ea738cde40ba2b3327e48fd8b4a48ee377
npksm <nick.pksm@gmail.com>
2020-01-14 01:30:52 +0530

    Update post.js

--- js/post.js
+++ js/post.js
@@ -1,13 +1,12 @@
+//function getPage
 
-
-function callPage(){
+function postImage(){
 	$.ajax({
 		url:"imagepost.html",
-		type: "GET",
+		type: "POST",
 		dataType: "text",
 			success: function (response){
 					console.log('the page was loaded', response);
-					$('.content').html(response);
 			},
 			error: function(error){
 					console.log('the page was NOT loaded', error);
