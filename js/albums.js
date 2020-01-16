81d9c4041c81f172f8f380a2cd61893a7c90f811
npksm <nick.pksm@gmail.com>
2020-01-16 20:29:09 +0530

    set a variable for html element

--- js/albums.js
+++ js/albums.js
@@ -9,9 +9,10 @@ var settings = {
 
 $.ajax(settings).done(function (response) {
 	console.log(response)	
+    var albumTitle = document.getElementById("title");
 				for (x in response.data){			
 					$('#albums').append('<a class="post"><!-- <div class="postid">'+response.data[x].link+'</div>--><img class="thumb" alt="'+response.data[x].title+'" src="https://i.imgur.com/'+response.data[x].cover+'m.jpg"><div class="title"></div></a>');
-                    $('.title').innerHTML = this.alt;
+                    albumTitle.innerHTML = this.alt;
 				}
 
 					//$(".thumb").click(function(){})
