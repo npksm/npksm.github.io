31a99b370400c9f36428cbe64a5b9ea8b68a371a
npksm <nick.pksm@gmail.com>
2020-01-17 21:52:14 +0530

    typos

--- js/post.js
+++ js/post.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
     var queryString = new Array();
-    $(function(){
-        if(queryString.length=0){
-            if (window.location.search.split('?').length > 1) {
-                var params = window.location.search.split('?')[1].split('&');
-                for (var i = 0; i < params.length; i++) {
-                    var key = params[i].split('=')[0];
-                    var value = decodeURIComponent(params[i].split('=')[1]);
-                    queryString[key] = value;
-		    }
+        $(function(){
+            if(queryString.length=0){
+                if (window.location.search.split('?').length > 1) {
+                    var params = window.location.search.split('?')[1].split('&');
+                        for (var i = 0; i < params.length; i++) {
+                            var key = params[i].split('=')[0];
+                            var value = decodeURIComponent(params[i].split('=')[1]);
+                                queryString[key] = value;
+		                }
+                }
+            }
         }
-
             var albumid= queryString["ID"];
-           
 });
 
 console.log(albumid)
