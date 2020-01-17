96b5acd5f06d4c3c276bad9fda4303b8cee50908
npksm <nick.pksm@gmail.com>
2020-01-17 22:03:40 +0530

    got rid of lots of ifs

--- js/post.js
+++ js/post.js
@@ -1,23 +1,18 @@
 $(document).ready(function(){
     var queryString = new Array();
-        $(function(){
-            if(queryString.length=0){
-                if (window.location.search.split('?').length > 1) {
-                    var params = window.location.search.split('?')[1].split('&');
-                        for (var i = 0; i < params.length; i++) {
-                            var key = params[i].split('=')[0];
-                            var value = decodeURIComponent(params[i].split('=')[1]);
-                                queryString[key] = value;
-		                }
-                }
-            }
-        }
+    var params = window.location.search.split('?')[1].split('&');
+
+    $(function(){              
+        for (var i = 0; i < params.length; i++) {
+            var key = params[i].split('=')[0];
+            var value = decodeURIComponent(params[i].split('=')[1]);
+            queryString[key] = value;
+	}                                 
             var albumid= queryString["ID"];
+            console.log(albumid)
 });
 
-console.log(albumid)
-
-var albumURL = "https://api.imgur.com/3/album/'+albumid+'/images"
+var albumURL = "https://api.imgur.com/3/album/'+albumid+'/images";
 
 var settings = {
   "url": albumURL,
