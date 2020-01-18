f6f92bc1e9650cc13159ea6ea82b4878b1793448
npksm <nick.pksm@gmail.com>
2020-01-18 10:50:13 +0530

    nope

--- js/albums.js
+++ js/albums.js
@@ -12,8 +12,8 @@ $.ajax(settings).done(function (response) {
    
 
 				for (x in response.data){			
-					$('#albums').append('<div id="albumtitle"><a class="post"><img class="thumb" src="https://i.imgur.com/'+response.data[x].cover+'m.jpg" data-source="'+response.data[x].id+'"></a></div>');
-                        $('#albumtitle').append("<p>"+response.data[x].title+"</p>")
+					$('#albums').append('<a class="post"><img class="thumb" src="https://i.imgur.com/'+response.data[x].cover+'m.jpg" data-source="'+response.data[x].id+'"></a>');
+                        //$('#albumtitle').append("<p>"+response.data[x].title+"</p>")
 				}
 
 					$(".thumb").click(function(){
