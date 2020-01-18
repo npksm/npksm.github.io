2299518055bc265b4a86a1dbcaee362f78f8879f
npksm <nick.pksm@gmail.com>
2020-01-18 19:51:51 +0530

    still broken

--- js/post.js
+++ js/post.js
@@ -32,11 +32,7 @@ $(document).ready(function(){
     var captionText = document.getElementById("caption");
     var modalImg = document.getElementById("modalImg");
 				for (x in response.data){			
-					$('#posts').append(
-                    '<a class="post">
-                        <div class="postid">'+response.data[x].id+'</div>
-                        <img class="thumb" alt="'+response.data[x].name+'" src="https://i.imgur.com/'+response.data[x].id+'m.jpg" data-source="https://i.imgur.com/'+response.data[x].id+'.jpg">
-                        </a>');
+					$('#posts').append('<a class="post"><div class="postid">'+response.data[x].id+'</div><img class="thumb" alt="'+response.data[x].name+'" src="https://i.imgur.com/'+response.data[x].id+'m.jpg" data-source="https://i.imgur.com/'+response.data[x].id+'.jpg"></a>');
 				}
              
     $(".thumb").click(function(){
