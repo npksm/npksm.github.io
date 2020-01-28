bcd3d700294dc611129f533e03d15a7b3cfcfc27
npksm <nick.pksm@gmail.com>
2020-01-28 11:13:02 +0530

    auth

--- js/gphotos.js
+++ js/gphotos.js
@@ -3,7 +3,7 @@ var settings = {
   "method": "GET",
   "timeout": 0,
   "headers": {
-    "Authorization": "Bearer 82019015804-1pmmkaagakpnqb10qls0s7elk4063p0s.apps.googleusercontent.com"
+    "Authorization": "82019015804-1pmmkaagakpnqb10qls0s7elk4063p0s.apps.googleusercontent.com"
   },
 };
 
