fe67b1a54f808523bf0d332388752954154b3549
npksm <nick.pksm@gmail.com>
2025-06-06 15:30:43 -0700

    nav update

--- js/gallery.js
+++ js/gallery.js
@@ -1,20 +1,41 @@
 $(document).ready(function () {
   getImages();
+  modalNavigationSetup();
   handleModalImageTrigger();
 });
 
+let imageList = [];
+let currentIndex = 0;
+
 function getImages() {
   $.getJSON("images.json", function (data) {
-    data.forEach(function (item) {
+    imageList = data;
+    data.forEach(function (item, index) {
       const full = item.src;
       const thumb = full.replace(
         /=w\d+-h\d+/,
         "=w200-h200"
       );
-      const imageHTML = `<img src="${thumb}" alt="${item.alt}" data-full="${full}"  class="gallery-img modal-trigger" />`;
+      const imageHTML = `<img src="${thumb}" alt="${item.alt}" data-full="${full}" data-index="${index}" class="gallery-img modal-trigger" />`;
       $("#imageContainer").append(imageHTML);
     });
   });
+}
+
+function modalNavigationSetup() {
+  $("#prevImage").on("click", showPreviousImage);
+  $("#nextImage").on("click", showNextImage);
+}
+function showPreviousImage() {
+  currentIndex =
+    (currentIndex - 1 + imageList.length) %
+    imageList.length;
+  showModalImage();
+}
+
+function showNextImage() {
+  currentIndex = (currentIndex + 1) % imageList.length;
+  showModalImage();
 }
 
 function handleModalImageTrigger() {
@@ -22,11 +43,17 @@ function handleModalImageTrigger() {
     "click",
     ".modal-trigger",
     function () {
-      const fullImg = $(this).data("full");
-      const imgAlt = $(this).data("alt");
-      $("#modalImage").attr("src", fullImg);
-      $("#modalImage").attr("alt", imgAlt);
-      $("#imageModal").modal("show");
+      currentIndex = parseInt($(this).data("index"));
+      showModalImage();
     }
   );
+}
+
+function showModalImage() {
+  const item = imageList[currentIndex];
+  if (item) {
+    $("#modalImage").attr("src", item.src);
+    $("#modalImage").attr("alt", item.alt);
+    $("#imageModal").modal("show");
+  }
 }
